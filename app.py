39720515e97a1816e4e778d84b6772009ac61c6d
darrendc-dev <ddalycarolan@hotmail.co.uk>
2021-02-14 13:01:40 +0000

    Added logout function

--- app.py
+++ app.py
@@ -85,7 +85,19 @@ def profile(username):
     # reading session user's username from cookies
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # removing "user" session cookie
+    flash("You been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
